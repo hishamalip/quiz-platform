d4532b08b31ac1d47640f02397ec65c3658df47e
hishamalip <hishamalip@gmail.com>
2020-06-22 21:54:24 +0530

    import folded changed

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -2,8 +2,8 @@ import React from 'react';
 import '../../Assets/css/landing/landing.css';
 
 import NavigationBar from './NavigationBar.js';
-import SigninModal from "./SigninModal.js";
-import SignupModal from "./SignupModal.js";
+import SigninModal from "./Signin/SigninModal.js";
+import SignupModal from "./Signup/SignupModal.js";
 
 export default class Landing extends React.Component {
     render() {
